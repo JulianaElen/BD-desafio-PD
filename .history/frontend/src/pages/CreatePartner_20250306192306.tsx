e23af84cb1491658357a48b7b22876f51095649d
JulianaElen <julianaesoliveira@gmail.com>
2025-03-06 21:17:13 -0300

    modificacao de arquivos

--- .history/frontend/src/pages/CreatePartner_20250306192306.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function CreatePartner() {
-  return (
-    <div>CreatePartner</div>
-  )
-}
--- .history/frontend/src/pages/SeePartner_20250306192323.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SeePartner() {
-  return (
-    <div>SeePartner</div>
-  )
-}
